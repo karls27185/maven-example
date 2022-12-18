5e4d1076cd0038edd4015feca8323e5c2bcdd209
karls27185 <karls27185@gmail.com>
2022-12-18 04:28:29 +0300

    Initial commit

---
+++ src/main/java/Launcher.java
@@ -0,0 +1,12 @@
+import org.apache.commons.lang3.SystemUtils;
+
+/**
+ * @author Karl
+ * @link https://babayan.keenetic.link/
+ */
+public class Launcher {
+
+    public static void main(String[] args) {
+        System.out.println(SystemUtils.getJavaHome());
+    }
+}
